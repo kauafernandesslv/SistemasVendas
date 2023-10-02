f83bfdfcb2df4050d88fa0524832cbd6f0c1e5f6
KAUA FERNANDES SILVA <kaua.1880@aluno.pr.senac.br>
2023-10-02 17:00:24 -0300

    first commit

---
+++ src/main/java/com/mycompany/sistemasvendas/SistemasVendas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemasvendas;
+
+/**
+ *
+ * @author kaua.1880
+ */
+public class SistemasVendas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
